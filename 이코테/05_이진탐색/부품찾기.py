e5caf91c8ba41380b56347ce78bf5aee2edc15b2
Lerry <lerry597@gmail.com>
2022-02-05 00:56:08 +0900

    [solve] 부품찾기.py

---
+++ 이코테/05_이진탐색/부품찾기.py
@@ -0,0 +1,25 @@
+def binary_search(array, target, start, end):
+  while start <= end:
+    mid = (start + end) // 2
+    if array[mid] == target:
+      return mid
+    elif array[mid] > target:
+      end = mid - 1
+    else:
+      start = mid + 1
+  return None
+  
+n = int(input())
+n_list = list(map(int, input().split()))
+n_list.sort()
+
+m = int(input())
+m_list = list(map(int, input().split()))
+
+print(n_list)
+for elem in m_list:
+  result = binary_search(n_list, elem, 0, n - 1)
+  if result == None:
+    print('no', end=' ')
+  else: 
+    print('yes', end=' ')
