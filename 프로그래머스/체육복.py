cd3cc92a696f888a4446b29823ed0b9e7500d679
lerrybe <lerry597@gmail.com>
2022-03-08 01:00:26 +0900

    [refactor] 체육복 리팩토링

--- 프로그래머스/체육복.py
+++ 프로그래머스/체육복.py
@@ -2,16 +2,8 @@ n = int(input())
 lost = list(map(int, input().split()))
 reserve = list(map(int, input().split()))
 
-lost.sort()
-reserve.sort()
-
-new_lost = [i for i in lost]
-new_reserve = [i for i in reserve]
-
-for student in reserve:
-  if student in lost:
-    new_lost.remove(student)
-    new_reserve.remove(student)
+new_lost = sorted([i for i in lost if i not in reserve])
+new_reserve = sorted([i for i in reserve if i not in lost])
 
 lost_to_reserved = 0
 
@@ -19,11 +11,9 @@ for target in new_lost:
   if (target - 1) in new_reserve:
     new_reserve.remove(target - 1)
     lost_to_reserved += 1
-    continue
-  if (target + 1) in new_reserve:
+  elif (target + 1) in new_reserve:
     new_reserve.remove(target + 1)
     lost_to_reserved += 1
-    continue
 
 answer = n - len(new_lost) + lost_to_reserved
 
