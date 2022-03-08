7b16ff8dc77836d20e68e56fcbc55f2633b8e2b4
lerrybe <lerry597@gmail.com>
2022-03-09 00:20:12 +0900

    [solve] 1343.py

---
+++ baekjoon/1343.py
@@ -0,0 +1,43 @@
+input_board = input()
+
+# 입력 -> 'XX.XX'
+# 출력 -> ['XX', '.', 'XX'] 
+def divide_board(board):
+    point = 0
+    count = 1
+    answer_lst = []
+    
+    if len(board) == 1:
+        return [board]
+
+    for i in range(len(board) - 1):
+        if board[i] == board[i + 1]:
+            count += 1
+        elif board[i] != board[i + 1]:
+            answer_lst.append(board[point:i + 1])
+            point = i + 1
+            count = 1
+
+    answer_lst.append(board[point:i + 2])
+    return answer_lst
+
+
+def solution(board):
+    result_lst = []
+
+    for target in divide_board(board):
+        if target[0] == '.':
+            result_lst.append(target)
+        else:
+            num = len(target)
+            if num % 4 == 0:
+                result_lst.append('A' * num)
+            elif num % 4 == 2:
+                result_lst.append('A' * (num // 4) * 4)
+                result_lst.append('BB')
+            else:
+                return -1
+
+    return ''.join(result_lst)
+
+print(solution(input_board))
